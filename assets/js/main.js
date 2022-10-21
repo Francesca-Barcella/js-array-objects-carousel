16f748c57eb3c052e9bd32e6aef562c795213e3b
Francesca-Barcella <pollon.fra@gmail>
2022-10-21 16:05:00 +0200

    JS - OC - stampate tutte le immagini visibili

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,74 @@ const images = [
 ];
 
 ****************************************************************************/
+
+//Milestone 1:
+// Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+// Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
+const slides = [
+    {
+        image: '01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, 
+    
+    {
+        image: '02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, 
+    
+    {
+        image: '03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, 
+    
+    {
+        image: '04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, 
+    
+    {
+        image: '05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+console.log(slides);
+
+
+// seleziono gli elementi della dom
+const slidesEl = document.querySelector('.slides');
+const precedenteEl = document.querySelector('.precedente');
+const successivaEl = document.querySelector('.successiva');
+console.log(slidesEl, precedenteEl, successivaEl);
+
+const srcStart = './assets/img/';
+
+for (let i = 0; i < slides.length; i++) {
+
+    const slide = slides[i];
+    const imgSrc = imageSrc(srcStart, slide.image);
+
+    const cardSlide = `
+     <div>
+        <img src="${imgSrc}"/>
+        <h4>${slide.title}</h4>
+        <h6>${slide.text}</h6>
+    </div>`;
+    console.log(cardSlide);
+
+    slidesEl.insertAdjacentHTML('beforeend', cardSlide);
+
+}
+
+
+function imageSrc(url_1, url_2) {
+    const totalSrc = (url_1 + url_2);
+    return totalSrc;
+}
+
+
